c001168cf4ac356229c3e99833cbc6934f143b44
Andrew Smith <me@andrewmichaelsmith.com>
2014-03-02 17:50:59 +0000

    okie dokie

--- source.php
+++ source.php
@@ -1,29 +1,14 @@
 <?php
-/**
-* shoutbox_pun
-*
-* https://github.com/andrewmichaelsmith/shoutbox_pun
-*
-*
-* Copyright (c) 2012 Andrew Smith
-*
-* Dual licensed under the MIT and GPL licenses:
-*   http://www.opensource.org/licenses/mit-license.php
-*   http://www.gnu.org/licenses/gpl.html
-*/
-
-define("MAX_PAGES", 5);
-
-ob_start("ob_gzhandler");
 header("Cache-Control: no-cache");
 header("Content-type: text/xml");  
-define('FORUM_ROOT', '../../');
+define('FORUM_ROOT', './../');
 
 define('FORUM_QUIET_VISIT', 1);
 
 
 require FORUM_ROOT.'include/common.php';
 
+echo "hllO"
 
 if($forum_user['id']!=1) {
  $query = "select
@@ -37,7 +22,7 @@ if($forum_user['id']!=1) {
           group by
               u1.username, u2.username";
 		
- $result = $forum_db->query_build($query) or error(__FILE__, __LINE__);
+ $result = $forum_db->query($query) or error(__FILE__, __LINE__);
 
   while ($s = $forum_db->fetch_assoc($result))
   {
