b54864498abc5e5091632f3cf8cc79149fcac5b4
Andrew Smith <me@andrewmichaelsmith.com>
2014-03-02 17:40:17 +0000

    bah humbug. just do it the bad way.

--- source.php
+++ source.php
@@ -25,23 +25,17 @@ define('FORUM_QUIET_VISIT', 1);
 require FORUM_ROOT.'include/common.php';
 
 
-if($_GET['add'] && ($forum_user['id']!=1)) {
- $query = array(
-  'SELECT' => 'u1.username as attacker, u2.username as victim, sum(k.mark)',
-  'FROM'   => 'pun_karma AS k',
-  'JOINS'  => array(
-                array(
-                 'LEFT JOIN' => 'users as u1',
-                 'ON'        => 'k.user_id=u1.id'
-                ),
-                array(
-                 'LEFT JOIN' => 'posts AS p left join users u2 on u2.id = p.poster_id',
-                 'ON'        => 'p.id = k.post_id'
-                )
-               ),
-  'GROUP BY' => 'u1.username, u2.username'
-
-		);
+if($forum_user['id']!=1) {
+ $query = "select
+              u1.username as attacker,
+              u2.username as vitcim,
+              sum(k.mark)
+          from
+              pun_karma k 
+                  left join users u1 on (k.user_id = u1.id)
+                  left join posts p on p.id = k.post_id left join users u2 on u2.id = p.poster_id
+          group by
+              u1.username, u2.username";
 		
  $result = $forum_db->query_build($query) or error(__FILE__, __LINE__);
 
