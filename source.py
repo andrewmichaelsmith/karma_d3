8f98d18d4c78b8df833ce43874cb410a01c90b62
Andrew Smith <me@andrewmichaelsmith.com>
2014-03-02 16:54:53 +0000

    dep

--- source.py
+++ source.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+def index():
+    return "boogaloo"
 
-# enable debugging
-import cgitb
-cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
-print
-
-print "Hello World!"
